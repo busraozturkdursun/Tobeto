640bffaf8ac47fd2cd3f931e8c5d63aafea5a6ca
Busra <124085115+busraozturkdursun@users.noreply.github.com>
2023-10-25 01:12:24 +0300

    Integer Veri Tipi

--- 1-C/CSharpCourse/TypesAndVariables/Program.cs
+++ 1-C/CSharpCourse/TypesAndVariables/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
+
+//Console.WriteLine("Hello, World!");
+
+//int: Tam sayılı bir veri tipidir. Value Types (değer tiplerinden) bir tanesidir.
+
+int number1 = 2147483647;
+Console.WriteLine("Number1 is {0}", number1);
+    
