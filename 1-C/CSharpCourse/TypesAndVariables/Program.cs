2d7cdaaf49c18656f58df0eb6bd59a72f363eb56
Busra <124085115+busraozturkdursun@users.noreply.github.com>
2023-10-25 01:21:52 +0300

    Long Veri Tipi

--- 1-C/CSharpCourse/TypesAndVariables/Program.cs
+++ 1-C/CSharpCourse/TypesAndVariables/Program.cs
@@ -5,6 +5,9 @@
 
 //int: Tam sayılı bir veri tipidir. Value Types (değer tiplerinden) bir tanesidir.
 
+
 int number1 = 2147483647;
+long number2 = -9223372036854775808;
 Console.WriteLine("Number1 is {0}", number1);
-    
+Console.WriteLine("Number2 is {0}", number2);
+
