ce65997814ca58ccde216e5665076f27ed3a830c
Busra <124085115+busraozturkdursun@users.noreply.github.com>
2023-10-25 01:28:49 +0300

    Short Veri Tipi

--- 1-C/CSharpCourse/TypesAndVariables/Program.cs
+++ 1-C/CSharpCourse/TypesAndVariables/Program.cs
@@ -5,9 +5,12 @@
 
 //int: Tam sayılı bir veri tipidir. Value Types (değer tiplerinden) bir tanesidir.
 
-
+short number3 = -32768;
 int number1 = 2147483647;
 long number2 = -9223372036854775808;
+
 Console.WriteLine("Number1 is {0}", number1);
 Console.WriteLine("Number2 is {0}", number2);
+Console.WriteLine("Number3 is {0}", number3);
+
 
