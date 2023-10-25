f0de52343d4fa8f324ce385391cdaa3403bdc397
Busra <124085115+busraozturkdursun@users.noreply.github.com>
2023-10-25 15:58:48 +0300

    Parametreli Metotlarla Çalışmak

--- 1-C/CSharpCourse/Methods/Program.cs
+++ 1-C/CSharpCourse/Methods/Program.cs
@@ -1,8 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Add();
+Add();
+Add();
+Add();
+var result = Add2(20,30);
+Console.WriteLine(result);
+
 
 static void Add()
 {
     Console.WriteLine("Added!");
+}
+
+
+static int Add2(int number1, int number2)
+{
+    var result = number1 + number2;
+    return result;
 }
