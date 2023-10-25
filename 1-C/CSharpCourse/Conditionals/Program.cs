dcd2d8850c66f5e6bb43b3d9e96fd42488de8d65
Busra <124085115+busraozturkdursun@users.noreply.github.com>
2023-10-25 15:24:43 +0300

    Çoklu Şartlarla Çalışmak

--- 1-C/CSharpCourse/Conditionals/Program.cs
+++ 1-C/CSharpCourse/Conditionals/Program.cs
@@ -14,7 +14,7 @@ else
 {
     Console.WriteLine("Number is not 10 or 20");
 }
-*/
+
 
 switch (number)
 {
@@ -29,4 +29,19 @@ switch (number)
     default:
         Console.WriteLine("Number is not 10 or 20");
         break;
+}
+
+*/
+
+if (number >= 0 && number <= 100)
+{
+    Console.WriteLine("Number is between 0-100");
+}
+else if (number>100 && number <= 200)
+{
+    Console.WriteLine("Number is between 101-200");
+}
+else if (number > 200 || number < 0)               // option  +  -  =  || (pipe) ya da anlamına gelir.
+{
+    Console.WriteLine("Number is less than 0 or greater than 200");
 }
