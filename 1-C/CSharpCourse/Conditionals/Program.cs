2aa072fd00481e0c7c12e7702e91e0334c2e95be
Busra <124085115+busraozturkdursun@users.noreply.github.com>
2023-10-25 13:06:03 +0300

    Single Line If İle Çalışmak

--- 1-C/CSharpCourse/Conditionals/Program.cs
+++ 1-C/CSharpCourse/Conditionals/Program.cs
@@ -1,11 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
 var number = 11;
-if (number ==10)
-{
-    Console.WriteLine("Number is 10");
-}
-else
-{
-    Console.WriteLine("Number is not 10");
-}
+Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");
+
+
