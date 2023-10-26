1a51887411a4968b5d123a3e3f047393735b517e
Busra <124085115+busraozturkdursun@users.noreply.github.com>
2023-10-27 02:12:32 +0300

    İnterfacelere Temel Giriş

--- 1-C/CSharpCourse/Classes/Customer.cs
+++ 1-C/CSharpCourse/Classes/Customer.cs
@@ -5,24 +5,23 @@ namespace Classes
 	{
       
         public int  Id { get; set; }
-
-
-        private string _firstName;
-        public string FirstName
-        {
-            get { return "Mrs." + _firstName; }
-            set {_firstName = value; }
-        }
-
-
+        public string FirstName { get; set; }
         public string LastName { get; set; }
         public string City { get; set; }
+
+
     }
 }
 
 
 
-
+/*  private string _firstName;
+  public string FirstName
+  {
+      get { return "Mrs." + _firstName; }
+      set {_firstName = value; }
+  }
+*/
 
 
 
---
+++ 1-C/CSharpCourse/Interfaces/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Interfaces;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
+
+interface IPerson
+{
+    int Id { get; set; }
+    int FirstName { get; set; }
+    int LastName { get; set; }
+
+
+}
+
+class Customer : IPerson
+{
+    //1. kullanım
+    public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public int FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public int LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    //2. kullanım
+    public string Address { get; set; }
+}
+
+class Student : IPerson
+{
+    public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public int FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public int LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public string Departmant { get; set; }
+
+}
