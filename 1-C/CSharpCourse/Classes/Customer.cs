37ca719a80d52ac5329426c5cc947a528f5ee613
Busra <124085115+busraozturkdursun@users.noreply.github.com>
2023-10-27 01:31:43 +0300

    Encapsulation

--- 1-C/CSharpCourse/Classes/Customer.cs
+++ 1-C/CSharpCourse/Classes/Customer.cs
@@ -3,12 +3,32 @@ namespace Classes
 {
 	public class Customer
 	{
+      
+        public int  Id { get; set; }
 
-		public int  Id { get; set; }
-        //public string FirstName { get; set; }
-        public string FirstName;
+
+        private string _firstName;
+        public string FirstName
+        {
+            get { return "Mrs." + _firstName; }
+            set {_firstName = value; }
+        }
+
+
         public string LastName { get; set; }
         public string City { get; set; }
     }
 }
 
+
+
+
+
+
+
+
+
+//field : değişken  (özellikleri tutmak için)
+//public string FirstName;
+
+//Propery :özellik tanımlamak için kullanılır.
