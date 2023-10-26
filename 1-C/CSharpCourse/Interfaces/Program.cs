af4d84569d4a78a3ba7a40a8a0f3382c708d98b6
Busra <124085115+busraozturkdursun@users.noreply.github.com>
2023-10-27 02:41:06 +0300

    İnterfaceleri Neden Kullanıyoruz

--- 1-C/CSharpCourse/Interfaces/Program.cs
+++ 1-C/CSharpCourse/Interfaces/Program.cs
@@ -4,36 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
+        PersonManager manager = new PersonManager();
+        manager.Add(new Customer { Id = 1, FirstName = "Erdinç", LastName = "Dursun", Address = "İstanbul" });
 
-    }
+        Student student = new Student
+        {
+            Id = 1,
+            FirstName = "Büşra",
+            LastName= "Öztürk",
+            Departmant= "Computer Scieneces"
+        };
+
+        manager.Add(student);
+        manager.Add(student);
+        Console.ReadLine();
 }
 
 interface IPerson
 {
     int Id { get; set; }
-    int FirstName { get; set; }
-    int LastName { get; set; }
-
-
+    string FirstName { get; set; }
+    string LastName { get; set; }
 }
 
 class Customer : IPerson
 {
-    //1. kullanım
-    public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-    public int FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-    public int LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-    //2. kullanım
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
     public string Address { get; set; }
 }
 
 class Student : IPerson
 {
-    public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-    public int FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-    public int LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
 
     public string Departmant { get; set; }
 
+}
+
+    class Student : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Departmant { get; set; }
+
+    }
+
+    class PersonManager
+{
+    public void Add(IPerson person)
+    {
+        Console.WriteLine(person.FirstName);
+    }
+}
 }
