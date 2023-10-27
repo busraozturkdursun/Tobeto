b8c1ef54851a6a1eac848ef3b9b00548855e3c0d
Busra <124085115+busraozturkdursun@users.noreply.github.com>
2023-10-27 04:07:26 +0300

    Kalıtıma Giriş

---
+++ 1-C/CSharpCourse/Inheritance/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Inheritance;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Customer customer = new Customer();
+        Person[] persons = new Person[3]
+        {
+            new Customer{FirtName = "Büşra"}, new Student{FirtName = "Erdinç"},
+            new Person{FirtName = "Nazlı"}
+        };
+
+        foreach (var person in persons)
+        {
+            Console.WriteLine(person.FirtName);
+        }
+        Console.ReadLine();
+    }
+}
+
+class Person
+{
+    public int Id { get; set; }
+    public string FirtName { get; set; }
+    public string LastName { get; set; }
+
+}
+
+class Customer :Person
+{
+    public string City { get; set; }
+}
+
+class Student : Person
+{
+    public string Department { get; set; }
+}
